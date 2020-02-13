9b3d9d64c08db4d3b4b07ab459165fc34b038313
Szabolcs Surányi <szabolcs.suranyi@shadesoft.hu>
2020-02-14 00:10:40 +0100

    Random generator + backtracking

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 let sudoku;
+let status;
+let gen;
 
 function setup() {
     createCanvas(310, 290);
-    noLoop();
+    //noLoop();
+    frameRate(2);
 
-    sudoku = new Sudoku;
+    sudoku = new Sudoku();
+    status = createP();
+    gen = 1;
 }
 
 function draw() {
@@ -14,7 +19,7 @@ function draw() {
 
     for(let row = 0; row < 9; row++) {
         for(let col = 0; col < 9; col++) {
-            text(sudoku.grid[row][col].value, (col + 1) * 30, (row + 1) * 30);
+            text(sudoku.grid[row][col] ? sudoku.grid[row][col] : '', (col + 1) * 30, (row + 1) * 30);
         }
     }
 
@@ -24,5 +29,22 @@ function draw() {
     line(0, 100, width, 100);
     line(0, 190, width, 190);
 
-    console.log(sudoku.grid[0][0].neighbors);
+    if(!sudoku.solving && sudoku.valid === null) {
+        sudoku.solving = true;
+        this.validate();
+    } else if(!sudoku.solving && sudoku.valid === false) {
+        sudoku.init();
+        gen++;
+    }
+
+    stroke(192);
+    status.html(
+        (sudoku.solving ? 'In progress' : (sudoku.valid ? 'Valid' : 'Invalid'))
+        + '<br>' + gen
+    );
+}
+
+async function validate() {
+    sudoku.valid = sudoku.solve();
+    sudoku.solving = false;
 }
--- sudoku.js
+++ sudoku.js
@@ -2,30 +2,75 @@ class Sudoku {
     constructor() {
         this.grid = [];
 
-        let options = [];
-        for(var i = 1; i <= 9; i++) {
-            options.push(i);
+        for(let row = 0; row < 9; row++) {
+            this.grid[row] = [];
+            for(let col = 0; col < 9; col++) {
+                this.grid[row][col] = 0;
+            }
         }
 
-        for(let i = 0; i < 9; i++) {
-            let row = [];
-            let available = options.slice();
-            for(let j = 0; j < 9; j++) {
-                let index = Math.floor(Math.random() * available.length);
-                let num = available[index];
-                available.splice(index, 1);
-                row.push(new Cell(i, j, num));
+        this.init(false);
+    }
+
+    init(fillZeroes = true) {
+        this.valid = null;
+        this.solving = false;
+
+        if(fillZeroes) {
+            for(let row = 0; row < 9; row++) {
+                for(let col = 0; col < 9; col++) {
+                    this.grid[row][col] = 0;
+                }
             }
-            this.grid.push(row);
+        }
+
+        for(let i = 0; i < 35; i++) {
+            let row, col, value;
+            while((value = this.grid[row = Math.floor(Math.random() * 9)][col = Math.floor(Math.random() * 9)]) !== 0);
+            while(!this.possible(row, col, value = Math.floor(Math.random() * 9) + 1));
+            this.grid[row][col] = value;
         }
     }
 
-    findInCol(i, j, num) {
-        for(let k = 0; k < i; k++) {
-            if(this.grid[k][j] == num) {
-                return true;
+    solve() {
+        for(let row = 0; row < 9; row++) {
+            for(let col = 0; col < 9; col++) {
+                if(this.grid[row][col] === 0) {
+                    for(let value = 1; value <= 9; value++) {
+                        if(this.possible(row, col, value)) {
+                            this.grid[row][col] = value;
+                            if(this.solve()) {
+                                return true;
+                            }
+                            this.grid[row][col] = 0;
+                        }
+                    }
+
+                    return false;
+                }
             }
         }
-        return false;
+
+        return true;
+    }
+
+    possible(row, col, value) {
+        for(let i = 0; i < 9; i++) {
+            if(this.grid[row][i] === value || this.grid[i][col] === value) {
+                return false;
+            }
+        }
+
+        let x = Math.floor(col / 3) * 3;
+        let y = Math.floor(row / 3) * 3;
+        for(let i = 0; i < 3; i++) {
+            for(let j = 0; j < 3; j++) {
+                if(this.grid[y + i][x + j] === value) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 }
